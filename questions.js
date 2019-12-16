6e532748016dd8a89db6b2fab5c00d414ccfa85f
Jake Walker <jcw2865@gmail.com>
2019-12-15 20:28:21 -0600

    Created initial files: index.html, style1.css, script.js, and questions.js. Linked js and css docs to index.html, as well as bootstrap style sheets.

---
+++ questions.js
@@ -0,0 +1,15 @@
+var questions = [
+    {
+        title: "Which of the following is a story the Jedi won't tell you?";
+        choices: ["The Jedi Civil War", "The Tragedy of Darth Plagueis the Wise", "The First Great Schism"];
+        answer: "The Tragedy of Darth Plagueis the Wise";
+    }
+    {
+        title: "Who was Darth Sidious's master?";
+        choices: ["Darth Tyrannous", "Darth Tenebrous", "Darth Plagueis"]
+        answer: "Darth Plagueis";
+    }
+    {
+        title: ""
+    }
+]

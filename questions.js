78b1a029bd07c2fbceb41f120145dc7811876548
Jake Walker <jcw2865@gmail.com>
2019-12-15 21:42:10 -0600

    Added initial structure to index.html. Created questions for questions.js.

--- questions.js
+++ questions.js
@@ -1,15 +1,27 @@
 var questions = [
     {
-        title: "Which of the following is a story the Jedi won't tell you?";
-        choices: ["The Jedi Civil War", "The Tragedy of Darth Plagueis the Wise", "The First Great Schism"];
-        answer: "The Tragedy of Darth Plagueis the Wise";
-    }
+        title: "Which of the following is a story the Jedi won't tell you?",
+        choices: ["The Jedi Civil War", "The Tragedy of Darth Plagueis the Wise", "The First Great Schism"],
+        answer: "The Tragedy of Darth Plagueis the Wise",
+    },
     {
-        title: "Who was Darth Sidious's master?";
-        choices: ["Darth Tyrannous", "Darth Tenebrous", "Darth Plagueis"]
-        answer: "Darth Plagueis";
-    }
+        title: "Which of Mace Windu's hands was cut off by Darth Sidious?",
+        choices: ["Left", "Right", "Both", "Neither"],
+        answer: "Right",
+    },
     {
-        title: ""
+        title: "Which of Luke Skywalker's hands was cut off by Darth Vader?",
+        choices: ["Left", "Right", "Both", "Neither"],
+        answer: "Right",
+    },
+    {
+        title: "What is the name of the creature that loses an arm to Luke Skywalker on Planet Hoth?",
+        choices: ["Wampa", "Tauntaun", "Rancor"],
+        answer: "Wampa",
+    },
+    {
+        title: "In which of the first six Star Wars movies does no one lose a hand?",
+        choices: ["Episode I", "Episode II", "Episode III", "Episode IV", "Episode V", "Episode VI"],
+        answer: "Episode I",
     }
 ]

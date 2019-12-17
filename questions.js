818699c28954b06b4f8d0c0386ebcbb09e19af4e
Jake Walker <jcw2865@gmail.com>
2019-12-16 22:56:15 -0600

    Changed HTML structure. Trying to figure out how to pull content out of object/array.

--- questions.js
+++ questions.js
@@ -1,27 +1,27 @@
 var questions = [
     {
-        title: "Which of the following is a story the Jedi won't tell you?",
+        question: "Which of the following is a story the Jedi won't tell you?",
         choices: ["The Jedi Civil War", "The Tragedy of Darth Plagueis the Wise", "The First Great Schism"],
-        answer: "The Tragedy of Darth Plagueis the Wise",
+        answer: 2,
     },
     {
-        title: "Which of Mace Windu's hands was cut off by Darth Sidious?",
+        question: "Which of Mace Windu's hands was cut off by Darth Sidious?",
         choices: ["Left", "Right", "Both", "Neither"],
-        answer: "Right",
+        answer: 2,
     },
     {
-        title: "Which of Luke Skywalker's hands was cut off by Darth Vader?",
+        question: "Which of Luke Skywalker's hands was cut off by Darth Vader?",
         choices: ["Left", "Right", "Both", "Neither"],
-        answer: "Right",
+        answer: 2,
     },
     {
-        title: "What is the name of the creature that loses an arm to Luke Skywalker on Planet Hoth?",
+        question: "What is the name of the creature that loses an arm to Luke Skywalker on Planet Hoth?",
         choices: ["Wampa", "Tauntaun", "Rancor"],
-        answer: "Wampa",
+        answer: 1,
     },
     {
-        title: "In which of the first six Star Wars movies does no one lose a hand?",
+        question: "In which of the first six Star Wars movies does no one lose a hand?",
         choices: ["Episode I", "Episode II", "Episode III", "Episode IV", "Episode V", "Episode VI"],
-        answer: "Episode I",
+        answer: 1,
     }
 ]
---
+++ script.js
@@ -0,0 +1,82 @@
+var questions = [
+    {
+        question: "Which of the following is a story the Jedi won't tell you?",
+        choices: ["The Jedi Civil War", "The Tragedy of Darth Plagueis the Wise", "The First Great Schism"],
+        answer: 2,
+    },
+    {
+        question: "Which of Mace Windu's hands was cut off by Darth Sidious?",
+        choices: ["Left", "Right", "Both", "Neither"],
+        answer: 2,
+    },
+    {
+        question: "Which of Luke Skywalker's hands was cut off by Darth Vader?",
+        choices: ["Left", "Right", "Both", "Neither"],
+        answer: 2,
+    },
+    {
+        question: "What is the name of the creature that loses an arm to Luke Skywalker on Planet Hoth?",
+        choices: ["Wampa", "Tauntaun", "Rancor"],
+        answer: 1,
+    },
+    {
+        question: "In which of the first six Star Wars movies does no one lose a hand?",
+        choices: ["Episode I", "Episode II", "Episode III", "Episode IV", "Episode V", "Episode VI"],
+        answer: 1,
+    }
+]
+
+// Declare variables
+
+var startButton = document.getElementById("startbutton");
+console.log(startButton);
+
+console.log(questions[1].question);
+
+var welcome = document.getElementById("welcome");
+
+startButton.addEventListener("click", function () {
+    // alert("You clicked the start button");
+    welcome.style.display = ("none");
+    document.getElementById("question").textContent = questions[1].question;
+    // console.log(questions[1].question);
+})
+
+
+// Below is what I started with. I copied and pasted it below in case I needed to reference it, but I believe what is written above is correct.
+
+
+// 
+// Establish variables
+// 
+
+// var startButton = document.getElementById("startbutton");
+// // console.log(startButton);
+// var row1 = document.getElementById("row-1");
+// // console.log(row1);
+// var questionSet1 = document.getElementById("questionset-1");
+// // console.log(question1);
+// var questionSet2 = document.getElementById("questionset-2");
+// // console.log(question2);
+// var questionSet3 = document.getElementById("questionset-3");
+// // console.log(question3);
+// var questionSet4 = document.getElementById("questionset-4");
+// // console.log(question4);
+// var questionSet5 = document.getElementById("questionset-5");
+// // console.log(question5);
+
+// 
+// Click event listeners
+// 
+
+// startButton.addEventListener("click", function () {
+//     // alert("You clicked the start button");
+//     row1.style.display = ("none");
+//     questionSet1.style.display = ("block");
+// })
+
+// document.querySelector("button").addEventListener("click", function () {
+//     // alert("You clicked the start button");
+//     questionSet1.style.display = ("none");
+//     questionSet2.style.display = ("block");
+// })
